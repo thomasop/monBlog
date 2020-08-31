47f5b521601d979c8f122166a621f2da588ca645
thomasop <tdss33@hotmail.com>
2020-08-31 16:22:49 +0200

    Edit: change Router, road, httprequest v2

--- App/tool/Router.php
+++ App/tool/Router.php
@@ -1,54 +1,35 @@
 <?php
 namespace App\tool;
-require('vendor/autoload.php');
-use App\controller\ControllerIndex;
-use App\controller\ControllerPost;
-use App\controller\ControllerComment;
-use App\controller\ControllerUser;
+
 use App\tool\Road;
 use PDO;
 use DOMDocument;
-
 use App\tool\HttpRequest;
 
-	class Router
+class Router
+{
+    private $_listroad;
+	
+	public function __construct()
 	{
-		private $_listroad;
-		
-		public function __construct()
-		{
-			$stringroad = file_get_contents('Config/route.json');
-			$this->_listroad = json_decode($stringroad);
-			
-		}
-		
-		public function findRoad($httprequest)
-		{
-			
-			$roadfound = array_filter($this->_listroad,function($road) use ($httprequest){
-				return preg_match("#^" . $road->path . "$#", $httprequest->getUrl()) && $road->method == $httprequest->getMethod();
-			});
-			//var_dump($this->_listroad);
-			$numberroad = count($roadfound);
-			
-			if($numberroad > 1)
-			{
-				echo 'no po';
-				
-			}
-			else if($numberroad == 0)
-			{
-				echo 'no';
-				
-			}
-			else
-			{
-				return new Road(array_shift($roadfound));	
-			}
+		$stringroad = file_get_contents('Config/route.json');
+		$this->_listroad = json_decode($stringroad);
+	}
+	
+	public function findRoad($httprequest)
+	{
+		$roadfound = array_filter($this->_listroad,function($road) use ($httprequest){
+			return preg_match("#^" . $road->path . "$#", $httprequest->getUrl()) && $road->method == $httprequest->getMethod();
+		});
+		$numberroad = count($roadfound);
+
+		if($numberroad > 1 || $numberroad == 0) {
+			echo 'error road';
+		} else {
+			return new Road(array_shift($roadfound));	
 		}
 	}
-
-
+}
 
 
 
